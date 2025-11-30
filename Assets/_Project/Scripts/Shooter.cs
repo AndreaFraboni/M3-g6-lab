eb9f5c6882154e8569c0af4e794e8f3c7bb2d70b
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2025-11-30 15:49:36 +0100

    Now can shoot bullet by mouse orietation

--- Assets/_Project/Scripts/Shooter.cs
+++ Assets/_Project/Scripts/Shooter.cs
@@ -6,42 +6,42 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] protected Bullet _bulletPrefab;
     [SerializeField] protected float _fireInterval = 0.5f;
+    [SerializeField] private float _offset = 1.5f;
+    [SerializeField] private AudioClip FireSound;
+
+    private AudioSource _AudioSource;
 
     protected float _lastShootTime;
+
+    private void Awake()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+        if (_AudioSource == null)
+        {
+            _AudioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public virtual void Shoot(Vector2 direction)
+    {
+        _lastShootTime = Time.time;
+
+        Bullet clonedBullet = Instantiate(_bulletPrefab);
+        clonedBullet.transform.position = transform.position + new Vector3(direction.x, direction.y, 0) * _offset;
+        clonedBullet.Shoot(direction);
+    }
 
     public virtual bool CanShootNow()
     {
         return Time.time - _lastShootTime > _fireInterval;
     }
 
-    public virtual void Shoot()
-    {
-        _lastShootTime = Time.time;
-
-        Bullet clonedBullet = Instantiate(_bulletPrefab);
-        clonedBullet.transform.position = transform.position + new Vector3(1.5f, 0, 0);
-        clonedBullet.Shoot(new Vector2(1, 0));
-    }
-
-
     public virtual void TryToShoot(Vector2 direction)
     {
         if (CanShootNow())
         {
-            Shoot();
-        }
-    }    
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (CanShootNow())
-            {
-                Shoot();
-            }
-        }
+            Shoot(direction);
+        }    
     }
-
 
 }
